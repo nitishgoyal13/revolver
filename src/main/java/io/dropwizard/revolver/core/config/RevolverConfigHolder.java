8272942a04d690a20e51e3c983d3014f189723b1
Jitendra Dhawan <jitendra.dhawan@phonepe.com>
2019-12-26 14:13:16 +0530

    adds atomic reference in revolver config holde

--- src/main/java/io/dropwizard/revolver/core/config/RevolverConfigHolder.java
+++ src/main/java/io/dropwizard/revolver/core/config/RevolverConfigHolder.java
@@ -1,16 +1,30 @@
 package io.dropwizard.revolver.core.config;
 
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.atomic.AtomicReference;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor
 public class RevolverConfigHolder {
 
-    private RevolverConfig config;
+    private AtomicReference<RevolverConfig> configRef;
+    private AtomicLong lastUpdatedTimestamp;
 
+    public RevolverConfigHolder(RevolverConfig initialRevolverConfig) {
+        this.configRef = new AtomicReference<>();
+        this.configRef.set(initialRevolverConfig);
+        this.lastUpdatedTimestamp = new AtomicLong(0L);
+        this.lastUpdatedTimestamp.set(System.currentTimeMillis());
+    }
+
+    public RevolverConfig getConfig() {
+        return configRef.get();
+    }
+
+    public void setConfig(RevolverConfig config) {
+        configRef.set(config);
+        lastUpdatedTimestamp.set(System.currentTimeMillis());
+    }
 }
