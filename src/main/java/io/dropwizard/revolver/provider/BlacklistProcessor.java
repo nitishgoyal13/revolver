066193b7c7a6c6621f3d19ba278f61843e6adba5
Jitendra Dhawan <jitendra.dhawan@phonepe.com>
2020-04-16 15:11:58 +0530

    refactors code

--- src/main/java/io/dropwizard/revolver/provider/BlacklistProcessor.java
+++ src/main/java/io/dropwizard/revolver/provider/BlacklistProcessor.java
@@ -3,6 +3,7 @@ package io.dropwizard.revolver.provider;
 import lombok.extern.slf4j.Slf4j;
 import org.glassfish.jersey.server.model.ModelProcessor;
 import org.glassfish.jersey.server.model.Resource;
+import org.glassfish.jersey.server.model.Resource.Builder;
 import org.glassfish.jersey.server.model.ResourceMethod;
 import org.glassfish.jersey.server.model.ResourceModel;
 
@@ -33,26 +34,18 @@ public class BlacklistProcessor implements ModelProcessor {
             final Resource.Builder resourceBuilder;
 
             final List<Resource> childResources = resource.getChildResources();
-            if (childResources.size() < 1) {
-                // No child methods present
-                resourceBuilder = Resource.builder(resource);
-            } else {
-                resourceBuilder = Resource.builder(resource.getPath());
-                for (final Resource childResource : childResources) {
-                    final Resource.Builder childResourceBuilder = Resource.builder(childResource.getPath());
-                    ResourceMethod.Builder methodBuilder = null;
-                    for (final ResourceMethod method : childResource.getResourceMethods()) {
-                        if (shouldAddMethod(method)) {
-                            methodBuilder = childResourceBuilder.addMethod(method);
-                        }
-                    }
-                    if (methodBuilder != null) {
-                        methodBuilder.build();
-                    }
 
+            resourceBuilder = childResources.isEmpty()
+                    ? Resource.builder(resource)
+                    : Resource.builder(resource.getPath());
+
+            childResources.forEach(childResource -> {
+                    final Builder childResourceBuilder = Resource.builder(childResource.getPath());
+                    childResource.getResourceMethods().stream()
+                            .filter(this::shouldAddMethod)
+                            .forEach(childResourceBuilder::addMethod);
                     resourceBuilder.addChildResource(childResourceBuilder.build());
-                }
-            }
+            });
 
             newResourceModelBuilder.addResource(resourceBuilder.build());
         }
@@ -66,23 +59,20 @@ public class BlacklistProcessor implements ModelProcessor {
     }
 
     private boolean shouldAddMethod(final ResourceMethod method) {
-        boolean shouldAdd = true;
-
         // This is slightly sub-optimal,
         // but given that blacklist count will be small, not creating a complex Map to match Jersey resource structure
-        for (final BlacklistMethodData blacklistData : blacklistMethods) {
-            if (method.getInvocable().getHandler().getHandlerClass().getName().equals(blacklistData.getResourceClassName()) &&
-                    method.getHttpMethod().equalsIgnoreCase(blacklistData.getHttpMethod()) &&
-                    method.getParent().getPath().equals(blacklistData.getRelativePath())) {
-                log.info("Blacklisting method with path: {}, http method: {}, resource: {}",
-                        blacklistData.getRelativePath(),
-                        blacklistData.getHttpMethod(),
-                        blacklistData.getResourceClassName());
-                shouldAdd = false;
-                break;
-            }
-        }
-
-        return shouldAdd;
+        return blacklistMethods.stream()
+                .noneMatch(blacklistData -> {
+                    if (method.getInvocable().getHandler().getHandlerClass().getName().equals(blacklistData.getResourceClassName()) &&
+                            method.getHttpMethod().equalsIgnoreCase(blacklistData.getHttpMethod()) &&
+                            method.getParent().getPath().equals(blacklistData.getRelativePath())) {
+                        log.info("Blacklisting method with path: {}, http method: {}, resource: {}",
+                                blacklistData.getRelativePath(),
+                                blacklistData.getHttpMethod(),
+                                blacklistData.getResourceClassName());
+                        return true;
+                    }
+                    return false;
+                });
     }
 }
